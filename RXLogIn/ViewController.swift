9034f2a84622742400f05816d290f5f140549b4c
Marina Aibulatova <marinka-dgerry@yandex.ru>
2021-08-09 13:41:49 +0500

    Initial Commit

---
+++ RXLogIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RXLogIn
+//
+//  Created by Марина Айбулатова on 09.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
