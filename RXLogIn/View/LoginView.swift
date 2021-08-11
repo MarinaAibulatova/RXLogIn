31d01ac2ebd4477e7f0a366d627987a330bacc7e
Marina Aibulatova <marinka-dgerry@yandex.ru>
2021-08-12 00:25:12 +0500

    fix view and controller

--- RXLogIn/View/LoginView.swift
+++ RXLogIn/View/LoginView.swift
@@ -9,197 +9,131 @@ import UIKit
 import SnapKit
 
 class LoginView: UIView {
-  
-  //MARK: - UI components
-   let mainView: UIView = {
-    let view = UIView()
-   
-    view.backgroundColor = .white
-    view.contentMode = .scaleToFill
-    view.isUserInteractionEnabled = true
     
-    return view
-  }()
-  
-   let emailTextField: UITextField = {
-    let textField = UITextField()
-    textField.font = UIFont.systemFont(ofSize: 14)
-    textField.placeholder = "Email"
-    textField.keyboardType = .emailAddress
-    textField.returnKeyType = .next
-    textField.autocapitalizationType = .none
-    textField.borderStyle = .roundedRect
-    textField.layer.borderColor = UIColor.systemTeal.cgColor
-    textField.layer.borderWidth = 1
-    textField.layer.cornerRadius = 10
-    textField.clipsToBounds = true
-    textField.backgroundColor = UIColor.systemGray6
-    textField.becomeFirstResponder()
-    
-    return textField
-  }()
-  
-   let passwordTextField: UITextField = {
-    let textField = UITextField()
-    textField.font = UIFont.systemFont(ofSize: 14)
-    textField.isSecureTextEntry = true
-    textField.placeholder = "Password"
-    textField.borderStyle = .roundedRect
-    textField.layer.borderColor = UIColor.systemTeal.cgColor
-    textField.layer.borderWidth = 1
-    textField.layer.cornerRadius = 10
-    textField.clipsToBounds = true
-    textField.backgroundColor = UIColor.systemGray6
-    
-    return textField
-  }()
-  
-   let forgotButton: UIButton = {
-    let button = UIButton()
-    button.backgroundColor = UIColor.white
-    button.setTitle("Forgot password", for: .normal)
-    button.setTitleColor(.systemTeal, for: .normal)
-    button.titleLabel?.font = UIFont.systemFont(ofSize: 10)
-   
-    return button
-  }()
-  
-   let logInButton: UIButton = {
-    let button = UIButton()
-    button.setTitle("Log in", for: .normal)
-    button.setTitleColor(UIColor.white, for: .normal)
-    button.setTitleColor(UIColor.systemBlue, for: .disabled)
-    button.setBackgroundColor(color: UIColor.systemGray6, forState: .disabled)
-    button.setBackgroundColor(color: UIColor.systemBlue, forState: .normal)
-    button.showsTouchWhenHighlighted = true
-    button.layer.cornerRadius = 10
-    
-    return button
-  }()
-  
-  let scrollView: UIScrollView = {
-    let scrollView = UIScrollView()
-    return scrollView
-  }()
-  
-  let contentView: UIView = {
-    let view = UIView()
+    //MARK: - public properties
+     var scrollView: UIScrollView!
+     var contentStackView: UIStackView!
+     var emailTextField: UITextField!
+     var passwordTextField: UITextField!
+     var forgotButton: UIButton!
+     var logInButton: UIButton!
 
-    return view
-  }()
-  
-  
-  //MARK: - init
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    setView()
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  //MARK: - set view
-  private func setView() {
-    self.backgroundColor = .white
+    //MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
     
-    addSubview(scrollView)
-    scrollView.addSubview(contentView)
-    contentView.addSubview(mainView)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
-    mainView.addSubview(emailTextField)
-    mainView.addSubview(passwordTextField)
-    mainView.addSubview(forgotButton)
-    mainView.addSubview(logInButton)
+    //MARK: - private methods
+    private func setupViews() {
+        backgroundColor = .white
+        
+        scrollView = {
+            let i = UIScrollView()
+            return i
+        }()
+        
+        contentStackView = {
+            let i = UIStackView()
+            i.axis = .vertical
+            i.spacing = 10
+            i.alignment = .fill
+            i.distribution = .fill
+            i.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+            i.isLayoutMarginsRelativeArrangement = true
+            return i
+        }()
+        
+        emailTextField = {
+            let i = UITextField()
+            i.font = UIFont.systemFont(ofSize: 14)
+            i.placeholder = "Email"
+            i.keyboardType = .emailAddress
+            i.returnKeyType = .next
+            i.autocapitalizationType = .none
+            i.borderStyle = .roundedRect
+            i.layer.borderColor = UIColor.systemTeal.cgColor
+            i.layer.borderWidth = 1
+            i.layer.cornerRadius = 10
+            i.clipsToBounds = true
+            i.backgroundColor = UIColor.systemGray6
+            i.becomeFirstResponder() // убрать в контроллер
+            return i
+        }()
+        
+        passwordTextField = {
+            let i = UITextField()
+            i.font = UIFont.systemFont(ofSize: 14)
+            i.isSecureTextEntry = true
+            i.placeholder = "Password"
+            i.borderStyle = .roundedRect
+            i.layer.borderColor = UIColor.systemTeal.cgColor
+            i.layer.borderWidth = 1
+            i.layer.cornerRadius = 10
+            i.clipsToBounds = true
+            i.backgroundColor = UIColor.systemGray6
+            return i
+        }()
+        
+        forgotButton = {
+            let i = UIButton()
+            i.backgroundColor = UIColor.white
+            i.setTitle("Forgot password", for: .normal)
+            i.setTitleColor(.systemTeal, for: .normal)
+            i.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+            return i
+        }()
+        
+        logInButton = {
+            let i = UIButton()
+            i.setTitle("Log in", for: .normal)
+            i.setTitleColor(UIColor.white, for: .normal)
+            i.setTitleColor(UIColor.systemBlue, for: .disabled)
+            i.setBackgroundColor(color: UIColor.systemGray6, forState: .disabled)
+            i.setBackgroundColor(color: UIColor.systemBlue, forState: .normal)
+            i.showsTouchWhenHighlighted = true
+            i.layer.cornerRadius = 10
+            return i
+        }()
+        
+        addSubview(scrollView)
+        scrollView.addSubview(contentStackView)
     
-    setScrollViewConstraints()
-    setContentViewConstraints()
-    setMainViewConstraints()
+        contentStackView.addArrangedSubview(emailTextField)
+        contentStackView.addArrangedSubview(passwordTextField)
+        contentStackView.addArrangedSubview(forgotButton)
+        contentStackView.addArrangedSubview(logInButton)
+    }
     
-    setEmailConstraints()
-    setPasswordConstraints()
-    setForgotButtonConstraints()
-    setLoginButtonConstraints()
-    
-  }
-  
-  //MARK: - constraints
-  private func setScrollViewConstraints() {
-    scrollView.snp.makeConstraints { (make) in
-      make.leading.equalToSuperview()
-      make.trailing.equalToSuperview()
-      make.bottom.equalToSuperview()
-      make.top.equalToSuperview()
+    private func setupConstraints() {
+        scrollView.snp.makeConstraints { (make) in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top)
+            make.left.bottom.right.equalTo(0)
+        }
+        
+        contentStackView.snp.makeConstraints { (make) in
+            make.edges.equalTo(0)
+            make.width.equalToSuperview()
+        }
     }
-  }
-  
-  private func setContentViewConstraints() {
-    contentView.snp.makeConstraints { (make) in
-      make.height.equalTo(UIScreen.main.bounds.height + 100)
-      make.leading.equalTo(scrollView.snp.leading)
-      make.trailing.equalTo(scrollView.snp.trailing)
-      make.bottom.equalTo(scrollView.snp.bottom)
-      make.top.equalTo(scrollView.snp.top)
-      make.width.equalTo(scrollView.snp.width)
-    }
-  }
-  
-  private func setMainViewConstraints() {
-    mainView.snp.makeConstraints { (make) in
-      make.leading.equalTo(contentView.snp.leading)
-      make.trailing.equalTo(contentView.snp.trailing)
-      make.top.equalTo(contentView.snp.top)
-    //  make.centerX.equalTo(contentView.snp.centerX)
-     // make.centerY.equalTo(contentView.snp.centerY)
-      make.height.equalTo(100)
-    }
-  }
-  
-  private func setEmailConstraints() {
-    emailTextField.snp.makeConstraints { (make) in
-      make.left.equalTo(mainView.layoutMargins.left)
-      make.right.equalTo(mainView.snp.right).offset(-8)
-      make.top.equalTo(mainView.safeAreaLayoutGuide.snp.top)
-      make.height.equalTo(35)
-    }
-  }
-  
-  private func setPasswordConstraints() {
-    passwordTextField.snp.makeConstraints { (make) in
-      make.left.equalTo(mainView.layoutMargins.left)
-      make.right.equalTo(mainView.snp.right).offset(-8)
-      make.top.equalTo(emailTextField.snp.bottom).offset(30)
-      make.height.equalTo(35)
-    }
-  }
-  
-  private func setForgotButtonConstraints() {
-    forgotButton.snp.makeConstraints { (make) in
-      make.top.equalTo(passwordTextField.snp.bottom).offset(20)
-      make.left.equalTo(mainView.layoutMargins.left)
-    }
-  }
-  
-  private func setLoginButtonConstraints() {
-    logInButton.snp.makeConstraints { (make) in
-      make.left.equalTo(mainView.layoutMargins.left)
-      make.right.equalTo(mainView.snp.right).offset(-8)
-      make.top.equalTo(forgotButton.snp.bottom).offset(30)
-    }
-  }
 }
 
 extension UIButton {
-  func setBackgroundColor(color: UIColor, forState: UIControl.State) {
-    self.clipsToBounds = true
-    UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
-    if let context = UIGraphicsGetCurrentContext() {
-      context.setFillColor(color.cgColor)
-      context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
-      let colorImage = UIGraphicsGetImageFromCurrentImageContext()
-      UIGraphicsEndImageContext()
-      self.setBackgroundImage(colorImage, for: forState)
+    func setBackgroundColor(color: UIColor, forState: UIControl.State) {
+        self.clipsToBounds = true
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+            let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            self.setBackgroundImage(colorImage, for: forState)
+        }
     }
-  }
 }
 
--- RXLogIn/ViewController.swift
+++ RXLogIn/ViewController.swift
@@ -10,102 +10,114 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-
- private let loginView = LoginView()
-  
-  //MARK: - rx
-  private let disposeBag = DisposeBag()
-  private var viewModel = LoginViewModel()
-  
-  //MARK: - ui
-  weak var emailTextField: UITextField!
-  weak var passwordTextField: UITextField!
-  weak var logInButton: UIButton!
-  weak var scrollView: UIScrollView!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
- 
-    setView()
-    bind()
-  }
-  
-  override func loadView() {
-    let view = LoginView()
-    self.emailTextField = view.emailTextField
-    self.passwordTextField = view.passwordTextField
-    self.logInButton = view.logInButton
-    self.scrollView = view.scrollView
     
-    self.view = view
-  }
-  
-  //MARK: - view
-  private func setView() {
-    title = "Log in"
-    emailTextField.delegate = self
-  
-    logInButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    //MARK: - private properties
+    private let disposeBag = DisposeBag()
+    private var viewModel = LoginViewModel()
     
-    NotificationCenter.default.addObserver(
-      self,
-      selector: #selector(keyboardWillShow(_:)),
-      name: UIResponder.keyboardWillShowNotification,
-      object: nil)
+    private weak var emailTextField: UITextField!
+    private weak var passwordTextField: UITextField!
+    private weak var logInButton: UIButton!
+    private weak var scrollView: UIScrollView!
     
-    NotificationCenter.default.addObserver(
-      self,
-      selector: #selector(keyboardWillHide(_:)),
-      name: UIResponder.keyboardWillHideNotification,
-      object: nil)
-  }
-
-  //MARK: - bind
-  func bind() {
-    emailTextField.rx.text
-      .orEmpty
-      .bind(to: viewModel.emailRelay)
-      .disposed(by: disposeBag)
-    
-    passwordTextField.rx.text
-      .orEmpty
-      .bind(to: viewModel.passwordRelay)
-      .disposed(by: disposeBag)
-
-    viewModel.validation()
-      .bind(to: self.logInButton.rx.isEnabled)
-      .disposed(by: disposeBag)
-    
-    
-  }
-
-  //MARK: - actions
-  @objc func buttonTapped(_ sender: UIButton) {
-    print("tap here")
-  }
-  
-  //MARK: - keybord scroll
-  @objc func keyboardWillShow(_ notification: Notification) {
-    setScrollViewInsetForKeyboardShow(true, notification: notification)
-  }
-  
-  @objc func keyboardWillHide(_ notification: Notification) {
-    setScrollViewInsetForKeyboardShow(false, notification: notification)
-  }
-  
-  func setScrollViewInsetForKeyboardShow(_ show: Bool, notification: Notification) {
-    guard
-      let userInfo = notification.userInfo,
-      let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
-      else {
-        return
+    //MARK: - life cicle
+    override func loadView() {
+        let view = LoginView()
+        self.emailTextField = view.emailTextField
+        self.passwordTextField = view.passwordTextField
+        self.logInButton = view.logInButton
+        self.scrollView = view.scrollView
+        
+        self.view = view
     }
-    scrollView.contentInset.bottom = (keyboardFrame.cgRectValue.height + 20) * (show ? 1: -1)
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Log in"
+        emailTextField.delegate = self
+        
+        configureViews()
+        configureBinding()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        registerKeybordNotification()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterKeybordNotification()
+    }
+    
+    //MARK: - private methods
+    private func configureViews() {
+        logInButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    }
+    
+    //MARK: - bind
+    func configureBinding() {
+        emailTextField.rx.text
+            .orEmpty
+            .bind(to: viewModel.emailRelay)
+            .disposed(by: disposeBag)
+        
+        passwordTextField.rx.text
+            .orEmpty
+            .bind(to: viewModel.passwordRelay)
+            .disposed(by: disposeBag)
+        
+        viewModel.validation()
+            .bind(to: self.logInButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+    }
+    
+    private func registerKeybordNotification() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+    
+    private func unregisterKeybordNotification() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    //MARK: - actions
+    @objc func buttonTapped(_ sender: UIButton) {
+        print("tap here")
+    }
+    
+    //MARK: - keybord scroll
+    @objc func keyboardWillShow(_ notification: Notification) {
+        setScrollViewInsetForKeyboardShow(true, notification: notification)
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        setScrollViewInsetForKeyboardShow(false, notification: notification)
+    }
+    
+    func setScrollViewInsetForKeyboardShow(_ show: Bool, notification: Notification) {
+        guard
+            let userInfo = notification.userInfo,
+            let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        else {
+            return
+        }
+        scrollView.contentInset.bottom = (keyboardFrame.cgRectValue.height + 20) * (show ? 1: -1)
+    }
 }
 
 extension ViewController: UITextFieldDelegate {
-  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    passwordTextField.becomeFirstResponder()
-  }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        passwordTextField.becomeFirstResponder()
+    }
 }
